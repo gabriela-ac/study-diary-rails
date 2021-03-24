00f1404b4f7c91caa12975a72fff54d9d9cd46e8
Gabriela Castro <gabriela.allcastro@gmail.com>
2021-03-24 16:07:49 -0300

    refatoração de código

--- app/controllers/study_items_controller.rb
+++ app/controllers/study_items_controller.rb
@@ -1,20 +1,39 @@
 class StudyItemsController < ApplicationController
+  before_action :set_study_item, only: [:show, :edit, :update, :destroy]
   def index
     @study_items = StudyItem.all
   end
   
-  def show
-    @study_item = StudyItem.find(params[:id])
-  end
+  def show; end
 
   def new
     @study_item = StudyItem.new
   end
 
   def create
-    @study_item = StudyItem.new(params.require(:study_item)
-                                      .permit(:title, :category, :description))
+    @study_item = StudyItem.new(study_item_params)
     @study_item.save
     redirect_to root_path
+  end
+
+  def edit; end
+
+  def update
+    @study_item.update(study_item_params)
+    redirect_to @study_item
+  end
+
+  def destroy
+    @study_item.destroy
+    redirect_to root_path
+  end
+
+  private
+  def set_study_item
+    @study_item = StudyItem.find(params[:id])
+  end
+
+  def study_item_params
+    params.require(:study_item).permit(:title, :category, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'study_items#index'
 
-  resources :study_items, only: [:index, :show, :new, :create]
+  resources :study_items
 end
